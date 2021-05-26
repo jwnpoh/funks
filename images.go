1deb36c5272aecb6bfa9723a16945decd9f5865c
jwnpoh <jwn.poh@gmail.com>
2021-05-26 17:31:22 +0800

    operations with images

--- images.go
+++ images.go
@@ -10,13 +10,13 @@ import (
 func LoadImage(f string) (image.Image, error) {
     file, err := os.Open(f)
     if err != nil {
-        return nil, fmt.Errorf("unable to open image file %s", f)
+        return nil, fmt.Errorf("unable to open image file %s: %w", f, err)
     }
     defer file.Close()
 
     img, _, err := image.Decode(file)
     if err != nil {
-        return nil, fmt.Errorf("unable to decode image file %s", f)
+        return nil, fmt.Errorf("unable to decode image file %s: %w", f, err)
     }
 
     return img, nil
