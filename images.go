3185b8d6f87f3a0553933d34620313454744517b
jwnpoh <jwn.poh@gmail.com>
2021-05-26 17:28:53 +0800

    operations with images

---
+++ images.go
@@ -0,0 +1,23 @@
+// Package funks implements basic functions that I use frequently to save time on replicating code across different projects.
+package funks
+
+import (
+	"fmt"
+	"image"
+	"os"
+)
+
+func LoadImage(f string) (image.Image, error) {
+    file, err := os.Open(f)
+    if err != nil {
+        return nil, fmt.Errorf("unable to open image file %s", f)
+    }
+    defer file.Close()
+
+    img, _, err := image.Decode(file)
+    if err != nil {
+        return nil, fmt.Errorf("unable to decode image file %s", f)
+    }
+
+    return img, nil
+}
